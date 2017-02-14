ae6971d0b233f7563145b61458101307a862e108
Uğur Artun <ugur.artun@sahibinden.com>
2017-02-14 21:45:36 +0200

    Initial commit

---
+++ src/main/java/com/ugur/microservices/MicroservicesbootApplication.java
@@ -0,0 +1,12 @@
+package com.ugur.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesbootApplication.class, args);
+	}
+}
